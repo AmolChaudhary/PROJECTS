141a07514614c458aecdac0ac6a4bbcd98a18fff
AmolChaudhary <133255956+AmolChaudhary@users.noreply.github.com>
2024-04-07 17:20:59 +0530

    Add files via upload

---
+++ e-commerce Website-Shop me now/search.js
@@ -0,0 +1,535 @@
+
+        const product = [
+            {
+                id: 0,
+                image: 'image/gg-1.jpg',
+                title: 'Z Flip Foldable Mobile',
+                price: 120,
+            },
+            {
+                id: 1,
+                image: 'image/hh-2.jpg',
+                title: 'Air Pods Pro',
+                price: 60,
+            },
+            {
+                id: 2,
+                image: 'image/ee-3.jpg',
+                title: '250D DSLR Camera',
+                price: 230,
+            },
+            {
+                id: 3,
+                image: 'image/aa-1.jpg',
+                title: 'Headphones',
+                price: 100,
+            },
+            {
+                id: 4,
+                image: 'image/bb-1.jpg',
+                title: 'Audio Microphone',
+                price: 230,
+            },
+            {
+                id: 5,
+                image: 'image/cc-1.jpg',
+                title: 'Smart Watch',
+                price: 100,
+            },
+            {
+                id: 6,
+                image: 'image/glass1.avif',
+                title: 'Men Black Glasses',
+                price: 110,
+            },
+            {
+                id: 7,
+                image: 'image/glass2.avif',
+                title: 'Men Brown Pattern Glasses',
+                price: 120,
+            },
+            {
+                id: 8,
+                image: 'image/glass3.avif',
+                title: 'Men Pink Shade Glasses',
+                price: 130,
+            },
+            {
+                id: 9,
+                image: 'image/glass4.jpg',
+                title: 'Men Retro Glasses',
+                price: 140,
+            },
+            {
+                id: 10,
+                image: 'image/glass5.avif',
+                title: 'Men Retro Glasses',
+                price: 150,
+            },
+            {
+                id: 11,
+                image: 'image/glass6.avif',
+                title: 'Men Glasses',
+                price: 160,
+            },
+            {
+                id: 12,
+                image: 'image/glass7.avif',
+                title: 'Men Metallic Glasses',
+                price: 170,
+            },
+            {
+                id: 13,
+                image: 'image/glass8.avif',
+                title: 'Brown Glasses',
+                price: 180,
+            },
+            {
+                id: 14,
+                image: 'image/hoodie1.jpg',
+                title: 'Green Hoodie',
+                price: 110,
+            },
+            {
+                id: 15,
+                image: 'image/hoodie2.avif',
+                title: 'White Hoodie',
+                price: 120,
+            },
+            {
+                id: 16,
+                image: 'image/hoodie3.avif',
+                title: 'Men Hoodie',
+                price: 130,
+            },
+            {
+                id: 17,
+                image: 'image/hoodie4.avif',
+                title: 'Grey hoodie',
+                price: 140,
+            },
+            {
+                id: 18,
+                image: 'image/hoodie5.avif',
+                title: 'Black hoodie',
+                price: 150,
+            },
+            {
+                id: 19,
+                image: 'image/hoodie6.avif',
+                title: 'Yellow Hoodie',
+                price: 160,
+            },
+            {
+                id: 20,
+                image: 'image/hoodie7.avif',
+                title: 'Yellow and Green Hoodie',
+                price: 170,
+            },
+            {
+                id: 21,
+                image: 'image/hoodie8.avif',
+                title: 'Red Hoodie',
+                price: 180,
+            },
+            {
+                id: 22,
+                image: 'image/p1.png',
+                title: 'Audio Microphone',
+                price: 110,
+            },
+            {
+                id: 23,
+                image: 'image/p2.png',
+                title: 'Smart Watch',
+                price: 120,
+            },
+            {
+                id: 24,
+                image: 'image/p3.png',
+                title: 'Z Flip Foldable Mobile',
+                price: 130,
+            },
+            {
+                id: 25,
+                image: 'image/p4.png',
+                title: 'Air Pods Pro',
+                price: 140,
+            },
+            {
+                id: 26,
+                image: 'image/p5.png',
+                title: '250D DSLR Camera',
+                price: 150,
+            },
+            {
+                id: 27,
+                image: 'image/p6.png',
+                title: 'Headphones',
+                price: 160,
+            },
+            {
+                id: 28,
+                image: 'image/p7.png',
+                title: 'Audio Microphone',
+                price: 170,
+            },
+            {
+                id: 29,
+                image: 'image/p8.png',
+                title: 'Smart Watch',
+                price: 180,
+            },
+            {
+                id: 30,
+                image: 'image/p9.jpg',
+                title: 'Z Flip Foldable Mobile',
+                price: 190,
+            },
+            {
+                id: 31,
+                image: 'image/p10.jpg',
+                title: 'Air Pods Pro',
+                price: 200,
+            },
+            {
+                id: 32,
+                image: 'image/p11.jpg',
+                title: '250D DSLR Camera',
+                price: 210,
+            },
+            {
+                id: 33,
+                image: 'image/p12.jpg',
+                title: 'Headphones',
+                price: 220,
+            },
+            {
+                id: 34,
+                image: 'image/p13.jpg',
+                title: 'Audio Microphone',
+                price: 230,
+            },
+            {
+                id: 35,
+                image: 'image/p14.png',
+                title: 'Smart Watch',
+                price: 240,
+            },
+            {
+                id: 36,
+                image: 'image/p15.png',
+                title: 'Z Flip Foldable Mobile',
+                price: 250,
+            },
+            {
+                id: 37,
+                image: 'image/p16.png',
+                title: 'Air Pods Pro',
+                price: 260,
+            },
+            {
+                id: 38,
+                image: 'image/p17.png',
+                title: '250D DSLR Camera',
+                price: 270,
+            },
+            {
+                id: 39,
+                image: 'image/p18.png',
+                title: 'Headphones',
+                price: 280,
+            },
+            {
+                id: 40,
+                image: 'image/p19.png',
+                title: 'Audio Microphone',
+                price: 290,
+            },
+            {
+                id: 41,
+                image: 'image/p20.png',
+                title: 'Smart Watch',
+                price: 300,
+            },
+            {
+                id: 42,
+                image: 'image/pant1.avif',
+                title: 'Z Flip Foldable Mobile',
+                price: 110,
+            },
+            {
+                id: 43,
+                image: 'image/pant2.avif',
+                title: 'Air Pods Pro',
+                price: 120,
+            },
+            {
+                id: 44,
+                image: 'image/pant3.avif',
+                title: '250D DSLR Camera',
+                price: 130,
+            },
+            {
+                id: 45,
+                image: 'image/pant4.avif',
+                title: 'Headphones',
+                price: 140,
+            },
+            {
+                id: 46,
+                image: 'image/pant5.jpg',
+                title: 'Audio Microphone',
+                price: 150,
+            },
+            {
+                id: 47,
+                image: 'image/pant6.avif',
+                title: 'Smart Watch',
+                price: 160,
+            },
+            {
+                id: 48,
+                image: 'image/pant7.avif',
+                title: 'Z Flip Foldable Mobile',
+                price: 170,
+            },
+            {
+                id: 49,
+                image: 'image/pant8.avif',
+                title: 'Air Pods Pro',
+                price: 180,
+            },
+            {
+                id: 50,
+                image: 'image/shoe1.avif',
+                title: '250D DSLR Camera',
+                price: 110,
+            },
+            {
+                id: 51,
+                image: 'image/shoe2.avif',
+                title: 'Headphones',
+                price: 120,
+            },
+            {
+                id: 52,
+                image: 'image/shoe3.avif',
+                title: 'Audio Microphone',
+                price: 130,
+            },
+            {
+                id: 53,
+                image: 'image/shoe4.avif',
+                title: 'Smart Watch',
+                price: 140,
+            },
+            {
+                id: 54,
+                image: 'image/shoe5.avif',
+                title: 'Headphones',
+                price: 150,
+            },
+            {
+                id: 55,
+                image: 'image/shoe6.avif',
+                title: 'Audio Microphone',
+                price: 160,
+            },
+            {
+                id: 56,
+                image: 'image/shoe7.avif',
+                title: 'Smart Watch',
+                price: 170,
+            },
+            {
+                id: 57,
+                image: 'image/shoe8.jpg',
+                title: 'Z Flip Foldable Mobile',
+                price: 180,
+            },
+            {
+                id: 58,
+                image: 'image/shoe1.avif',
+                title: '250D DSLR Camera',
+                price: 110,
+            },
+            {
+                id: 59,
+                image: 'image/shoe2.avif',
+                title: 'Headphones',
+                price: 120,
+            },
+            {
+                id: 60,
+                image: 'image/shoe3.avif',
+                title: 'Audio Microphone',
+                price: 130,
+            },
+            {
+                id: 61,
+                image: 'image/shoe4.avif',
+                title: 'Smart Watch',
+                price: 140,
+            },
+            {
+                id: 62,
+                image: 'image/pant4.avif',
+                title: 'Headphones',
+                price: 150,
+            },
+            {
+                id: 63,
+                image: 'image/pant5.jpg',
+                title: 'Audio Microphone',
+                price: 160,
+            },
+            {
+                id: 64,
+                image: 'image/pant6.avif',
+                title: 'Smart Watch',
+                price: 170,
+            },
+            {
+                id: 65,
+                image: 'image/pant7.avif',
+                title: 'Z Flip Foldable Mobile',
+                price: 180,
+            },
+            {
+                id: 66,
+                image: 'image/pant8.avif',
+                title: 'Air Pods Pro',
+                price: 190,
+            },
+            {
+                id: 67,
+                image: 'image/tshirt1.avif',
+                title: '250D DSLR Camera',
+                price: 110,
+            },
+            {
+                id: 68,
+                image: 'image/tshirt2.avif',
+                title: 'Headphones',
+                price: 120,
+            },
+            {
+                id: 69,
+                image: 'image/tshirt3.avif',
+                title: 'Audio Microphone',
+                price: 130,
+            },
+            {
+                id: 70,
+                image: 'image/tshirt3.jpg',
+                title: 'Smart Watch',
+                price: 140,
+            },
+            {
+                id: 71,
+                image: 'image/t-shirt5.avif',
+                title: 'Headphones',
+                price: 150,
+            },
+            {
+                id: 72,
+                image: 'image/t-shirt6.avif',
+                title: 'Audio Microphone',
+                price: 160,
+            },
+            {
+                id: 73,
+                image: 'image/t-shirt7.avif',
+                title: 'Smart Watch',
+                price: 170,
+            },
+            {
+                id: 74,
+                image: 'image/t-shirt8.avif',
+                title: 'Z Flip Foldable Mobile',
+                price: 180,
+            },
+            {
+                id: 75,
+                image: 'image/watch1.avif',
+                title: 'Air Pods Pro',
+                price: 110,
+            },
+            {
+                id: 76,
+                image: 'image/watch2.avif',
+                title: '250D DSLR Camera',
+                price: 120,
+            },
+            {
+                id: 77,
+                image: 'image/watch3.avif',
+                title: 'Headphones',
+                price: 130,
+            },
+            {
+                id: 78,
+                image: 'image/watch4.avif',
+                title: 'Audio Microphone',
+                price: 140,
+            },
+            {
+                id: 79,
+                image: 'image/watch5.avif',
+                title: 'Smart Watch',
+                price: 150,
+            },
+            {
+                id: 80,
+                image: 'image/watch6.avif',
+                title: 'Smart Watch',
+                price: 160,
+            },
+            {
+                id: 81,
+                image: 'image/watch7.avif',
+                title: 'Smart Watch',
+                price: 170,
+            },
+            {
+                id: 82,
+                image: 'image/watch8.avif',
+                title: 'Smart Watch',
+                price: 180,
+            },
+        ];
+
+        const categories = [...new Set(product.map((item) => { return item }))]
+
+        document.getElementById('searchBar').addEventListener('keyup', (e) => {
+            const searchData = e.target.value.toLowerCase();
+            const filteredData = categories.filter((item) => {
+                return (
+                    item.title.toLowerCase().includes(searchData)
+                )
+            })
+            displayItem(filteredData)
+        });
+
+        const displayItem = (items) => {
+            document.getElementById('root').innerHTML = items.map((item) => {
+                var { image, title, price } = item;
+                return (
+                    `<div class='box'>
+                        <div class='img-box'>
+                            <img class='images' src=${image}></img>
+                        </div> 
+                        <div class='bottom'>
+                            <p class='par'>${title}</p>
+                            <h2>$ ${price}.00</h2>
+                        <button class='but'>Add to cart</button>
+                        </div>
+                    </div>`
+                )
+            }).join('')
+        };
+        displayItem(categories);
+
+    
+
